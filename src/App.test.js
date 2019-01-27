39c7f6b5a390a03d8291aabcb5f3cf9031d9b251
thamrins <tsgold@gmail.com>
2019-01-27 15:03:43 +0700

    edit app.test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const initialState = {
+    count: 0
+}
+
+function reducer(state=initialState, action){
+    switch(action.type){
+        case 'INCREMENT':
+        return {
+            count: state.count + 1
+        }
+        case 'DECREMENT':
+        return {
+            count: state.count - 1
+        }
+        case 'INCREMENTDUA':
+        return {
+            count: state.count + action.plus
+        }
+        default:
+         return state
+    }
+}
+
+const store = createStore(reducer);
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
